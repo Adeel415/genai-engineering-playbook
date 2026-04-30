97d5ab18b99752b06e6068c5331292547f1a1ef2
Muhammad Adeel Abid <cmkali2126@gmail.com>
2026-04-30 20:09:49 +0500

    Python basics (Datatypes and if else)

---
+++ 01_python_core/basics/DataTypes.py
@@ -0,0 +1,18 @@
+#DAtaTypes 
+name="Adeel"
+age=22
+height=5.8
+is_student=True
+
+print(f"Name is {name}\n Age is {age} \n Height is {height}")
+
+#INput OUTUT
+
+name=input("ENter Your NAme --- ")
+
+print(name)
+
+age=int(input("Enter your age -- "))
+print(age)
+
+
---
+++ 01_python_core/basics/if_else.py
@@ -0,0 +1,23 @@
+age=int(input("ENter Your Age -- "))
+
+if age>=18:
+    print("You are Adult")
+else:
+    print("You are Child")
+
+
+#A bit Advance one
+print("Grade Calculation--")
+marks=int(input("ENter your marks -- "))
+
+if marks>=86:
+    print("A")
+elif marks>=80:
+    print("A-")
+elif marks>=74:
+    print("B+")
+elif marks>=65:
+    print("C")
+
+else:
+    print("F")        
